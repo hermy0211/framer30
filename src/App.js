133fbdfee83637a349b23faa583ced14e003a896
Anne Lee <hermy0211@naver.com>
2018-10-11 21:25:53 +0900

    Full functionality of menus

--- src/App.js
+++ src/App.js
@@ -11,19 +11,26 @@ import iphone from './img/iphone.svg'
 
 class App extends Component {
   state = {
-    windowHeight: window.innerHeight,
-    currentMenu: {}
+    windowHeight: window.innerHeight
   }
 
   componentDidMount = () => {
     this.scrollAnimation()
-    this.controlScroll()
-    this.selectMenu()
+    this.sectionSize()
+
+    let menuItems = document.querySelectorAll(".menu-item")
+    let elements = [...menuItems]
+
+    scroll.scrollTo(1)
+
+    elements.forEach((element) => {
+      element.classList.add("color-white")
+    })
   }
 
   scrollAnimation = () => {
     let app = document.querySelector(".App")
-    let menuItems = document.querySelectorAll(".original")
+    let menuItems = document.querySelectorAll(".menu-item")
     let elements = [...menuItems]
     let scroll = document.querySelector(".mouse")
     let scrollText = document.querySelector(".scrolltext")
@@ -31,7 +38,6 @@ class App extends Component {
     let detailItems = document.querySelectorAll(".details")
     let details = [...detailItems]
     let windowHeight = this.state.windowHeight
-    let about = document.querySelector(".about")
 
     window.addEventListener('scroll', () => {
       let y = window.scrollY
@@ -49,8 +55,6 @@ class App extends Component {
         details.forEach((detail) => {
           detail.classList.remove("display-none")
         })
-        about.classList.add("bg-color-white")
-        about.classList.remove("bg-color-trans")
       } else {
         app.classList.add("bg-color-black")
         app.classList.remove("bg-color-white")
@@ -65,18 +69,15 @@ class App extends Component {
         details.forEach((detail) => {
           detail.classList.add("display-none")
         })
-        about.classList.remove("bg-color-white")
-        about.classList.add("bg-color-trans")
       }
     })
   }
 
-  controlScroll = () => {
+  sectionSize = () => {
     let selectSections = document.querySelectorAll(".section")
     let sections = [...selectSections]
     let allSections = document.querySelector(".sections")
     let i = 0
-    let currentSection = 1
     let windowHeight = this.state.windowHeight
 
     for (let section of sections) {
@@ -89,20 +90,10 @@ class App extends Component {
     window.addEventListener('keydown', (event) => {
       if(event.keyCode === 40) {
         event.preventDefault()
-        if (currentSection < sections.length) {
-          scroll.scrollTo(windowHeight*(currentSection), {duration: 400})
-          currentSection++
-        } else if (currentSection = sections.length) {
-          scroll.scrollTo(windowHeight*(currentSection), {duration: 400})
-        }
+        scroll.scrollMore(windowHeight, {duration: 400})
       } else if (event.keyCode === 38) {
         event.preventDefault()
-        if (currentSection > 1) {
-          scroll.scrollTo(windowHeight*(currentSection-2), {duration: 400})
-          currentSection--
-        } else if (currentSection = 1) {
-          scroll.scrollTo(windowHeight*(currentSection-2), {duration: 400})
-        }
+        scroll.scrollMore(-windowHeight, {duration: 400})
       } else if (event.keyCode === 32) {
         event.preventDefault()
       }
@@ -116,22 +107,12 @@ class App extends Component {
       const delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
       console.log(delta)
 
-      if (delta == -1) {
+      if (delta === -1) {
         event.preventDefault()
-        if (currentSection < sections.length) {
-          scroll.scrollTo(windowHeight*(currentSection), {duration: 400})
-          currentSection++
-        } else if (currentSection = sections.length) {
-          scroll.scrollTo(windowHeight*(currentSection), {duration: 400})
-        }
-      } else if (delta == 1) {
+        scroll.scrollMore(windowHeight, {duration: 400})
+      } else if (delta === 1) {
         event.preventDefault()
-        if (currentSection > 1) {
-          scroll.scrollTo(windowHeight*(currentSection-2), {duration: 400})
-          currentSection--
-        } else if (currentSection = 1) {
-          scroll.scrollTo(windowHeight*(currentSection-2), {duration: 400})
-        }
+        scroll.scrollMore(-windowHeight, {duration: 400})
       } else {
         event.preventDefault()
       }
@@ -144,58 +125,6 @@ class App extends Component {
     })
   }
 
-  selectMenu = () => {
-    const menuItems = document.querySelectorAll(".menu-item")
-    const menus = [...menuItems]
-    const menuOne = document.querySelector(".menu-one")
-    const menuTwo = document.querySelector(".menu-two")
-    const menuThree = document.querySelector(".menu-three")
-    const menuFour = document.querySelector(".menu-four")
-    const menuFive = document.querySelector(".menu-five")
-    const menuSix = document.querySelector(".menu-six")
-    const menuSeven = document.querySelector(".menu-seven")
-    let windowHeight = this.state.windowHeight
-
-    this.setState({currentMenu: document.querySelector(".selected")})
-
-    menus.forEach((menu) => {
-      menu.addEventListener('click', () => {
-        let currentMenu = this.state.currentMenu
-        if (menu.classList.contains("original")) {
-          menu.classList.add("selected")
-          menu.classList.remove("original")
-          currentMenu.classList.add("original")
-          currentMenu.classList.remove("selected")
-          this.setState({currentMenu: menu})
-        } else {
-          return
-        }
-      })
-    })
-
-    menuOne.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*0, {duration: 1000})
-    })
-    menuTwo.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*1, {duration: 1000})
-    })
-    menuThree.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*2, {duration: 1000})
-    })
-    menuFour.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*3, {duration: 1000})
-    })
-    menuFive.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*4, {duration: 1000})
-    })
-    menuSix.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*5, {duration: 1000})
-    })
-    menuSeven.addEventListener('click', () => {
-      scroll.scrollTo(windowHeight*6, {duration: 1000})
-    })
-  }
-
   render() {
     return (
       <div className="App bg-color-black">
@@ -203,55 +132,122 @@ class App extends Component {
           <p>Please view this page on a PC.</p>
         </div>
         <div className="sections">
-          <section className="main section">
-            <div className="info">
-              <div className="title">
-                <div className="framer">FramerX</div>
-                <img src={cross}/>
-                <div className="react">React</div>
-              </div>
-              <div className="subtitle">
-                30 Prototypes in 30 Days
-              </div>
-              <div className="tags">
-                <div className="tag">#Framer30</div>
-                <div className="tag">#Prototyping</div>
-                <div className="tag">#UXUIDesign</div>
-              </div>
-              <div className="scroll">
-                <div className="mouse">
-                  <img src={mouse} className="mousecircle"/>
-                  <img src={mousescroll} className="mousescroll"/>
+          <Element
+            name="sectionOne"
+            className="section">
+            <section className="main inner-section">
+              <div className="info">
+                <div className="title">
+                  <div className="framer">FramerX</div>
+                  <img src={cross} alt="cross"/>
+                  <div className="react">React</div>
                 </div>
-                <div className="scrolltext">Scroll Down</div>
+                <div className="subtitle">
+                  30 Prototypes in 30 Days
+                </div>
+                <div className="tags">
+                  <div className="tag">#Framer30</div>
+                  <div className="tag">#Prototyping</div>
+                  <div className="tag">#UXUIDesign</div>
+                </div>
+                <div className="scroll">
+                  <div className="mouse">
+                    <img src={mouse} className="mousecircle" alt="mouse"/>
+                    <img src={mousescroll} className="mousescroll" alt="mouse wheel"/>
+                  </div>
+                  <div className="scrolltext">Scroll Down</div>
+                </div>
               </div>
-            </div>
-          </section>
-          <Section/>
-          <Section/>
-          <Section/>
-          <Section/>
-          <Section/>
-          <Section/>
+            </section>
+          </Element>
+          <Element
+            name="sectionTwo"
+            className="section"
+          ><Section/></Element>
+          <Element
+            name="sectionThree"
+            className="section"
+          ><Section/></Element>
+          <Element
+            name="sectionFour"
+            className="section"
+          ><Section/></Element>
+          <Element
+            name="sectionFive"
+            className="section"
+          ><Section/></Element>
+          <Element
+            name="sectionSix"
+            className="section"
+          ><Section/></Element>
+          <Element
+            name="sectionSeven"
+            className="section"
+          ><Section/></Element>
         </div>
         <div className="menus">
           <div className="sidemenu">
-            <div className="menu-item menu-one selected">Home</div>
-            <div className="menu-item menu-two original color-white">01 - 05</div>
-            <div className="menu-item menu-three original color-white">06 - 10</div>
-            <div className="menu-item menu-four original color-white">11 - 15</div>
-            <div className="menu-item menu-five original color-white">16 - 20</div>
-            <div className="menu-item menu-six original color-white">21 - 25</div>
-            <div className="menu-item menu-seven original color-white">25 - 30</div>
+            <Link
+              to="sectionOne"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">
+              Home
+            </Link>
+            <Link
+              to="sectionTwo"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">
+              01 - 05</Link>
+            <Link
+              to="sectionThree"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">06 - 10</Link>
+            <Link
+              to="sectionFour"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">11 - 15</Link>
+            <Link
+              to="sectionFive"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">16 - 20</Link>
+            <Link
+              to="sectionSix"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">21 - 25</Link>
+            <Link
+              to="sectionSeven"
+              activeClass="selected"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              className="menu-item">25 - 30</Link>
           </div>
           <div>
-            <button type="button" name="About" className="about bg-color-trans">About</button>
+            <button type="button" name="About" className="about">About</button>
           </div>
           <div className="copyright">
             © Anne Lee, 2018
           </div>
         </div>
-        <img src={iphone} className="iphone display-none"/>
+        <img src={iphone} className="iphone display-none" alt="iphone"/>
       </div>
     );
   }
--- src/Section.js
+++ src/Section.js
@@ -6,13 +6,13 @@ import github from './img/github.png'
 class Section extends Component {
   render() {
     return (
-      <section className="section">
+      <section className="inner-section">
         <div className="details display-none">
           <p className="day">DAY 01</p>
           <p className="title">Safari Scroll Interaction</p>
           <p className="description">A simple, Safari style scroll based on the webpage panning interaction. Created with Framer X and React.js. A simple, Safari style scroll based on the webpage panning interaction.</p>
-          <a href="https://medium.com/@annelee_22736" target="_blank" rel="noopener noreferrer" className="logo-wrapper" tabindex="-1"><img src={medium} className="logo" alt="medium logo"/></a>
-          <a href="https://github.com/hermy0211" target="_blank" rel="noopener noreferrer" className="logo-wrapper" tabindex="-1"><img src={github} className="logo" alt="github logo"/></a>
+          <a href="https://medium.com/@annelee_22736" target="_blank" rel="noopener noreferrer"><img src={medium} className="logo" alt="medium logo"/></a>
+          <a href="https://github.com/hermy0211" target="_blank" rel="noopener noreferrer"><img src={github} className="logo" alt="github logo"/></a>
         </div>
       </section>
     )
